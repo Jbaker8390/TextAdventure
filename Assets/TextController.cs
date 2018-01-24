fdbb8db711b73387895014c648289573091e7f7c
Jbaker8390 <jonathantuan02@gmail.com>
2018-01-23 23:23:16 -0800

    added nav in cell to other rooms

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -22,12 +22,12 @@ public class TextController : MonoBehaviour
         print(myState);
         if (myState == States.cell)          { state_cell(); }
         else if (myState == States.sheets_0) { state_sheets_0(); }
-        else if (myState == States.sheets_0) { state_sheets_1(); }
-        else if (myState == States.sheets_0) { state_lock_0(); }
-        else if (myState == States.sheets_0) { state_lock_1(); }
-        else if (myState == States.sheets_0) { state_mirror(); }
-        else if (myState == States.sheets_0) { state_cell_mirror(); }
-        else if (myState == States.sheets_0) { state_freedom(); }
+        else if (myState == States.sheets_1) { state_sheets_1(); }
+        else if (myState == States.lock_0) { state_lock_0(); }
+        else if (myState == States.lock_1) { state_lock_1(); }
+        else if (myState == States.mirror) { state_mirror(); }
+        else if (myState == States.cell_mirror) { state_cell_mirror(); }
+        else if (myState == States.freedom) { state_freedom(); }
 
     }
 
@@ -40,6 +40,12 @@ public class TextController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S))
         {
             myState = States.sheets_0;
+        }else if (Input.GetKeyDown(KeyCode.M))
+        {
+            myState = States.mirror;
+        }else if(Input.GetKeyDown(KeyCode.L))
+        {
+            myState = States.lock_0;
         }
 
     }
